fdb154690c8465f4931ec52d9c3f5c36f029cf59
JoeyZhou <joeyzhou@joeypro.gateway>
2017-01-14 23:23:47 +1030

    configured the circle pop up menu for the category selection, and display the chosen category to user

--- Podfile
+++ Podfile
@@ -8,6 +8,8 @@ target 'AroundMe' do
   # Pods for AroundMe
  pod 'SkyFloatingLabelTextField', '~> 2.0.0'
  pod 'AnimatedTextInput'
+ pod 'CircleMenu', '~> 2.0.1'
+
   target 'AroundMeTests' do
     inherit! :search_paths
     # Pods for testing
--- Pods/Target Support Files/Pods-AroundMe/Pods-AroundMe-frameworks.sh
+++ Pods/Target Support Files/Pods-AroundMe/Pods-AroundMe-frameworks.sh
@@ -85,9 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AnimatedTextInput/AnimatedTextInput.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/CircleMenu/CircleMenu.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SkyFloatingLabelTextField/SkyFloatingLabelTextField.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AnimatedTextInput/AnimatedTextInput.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/CircleMenu/CircleMenu.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SkyFloatingLabelTextField/SkyFloatingLabelTextField.framework"
 fi
